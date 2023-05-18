0a495967ace8369e8854aba6753f2adac250edee
MaryamTalebpour <maryamtalebpourb@gmail.com>
2023-05-18 15:37:17 +0330

    .

--- scripts/shared.js
+++ scripts/shared.js
@@ -2,7 +2,7 @@ let backdrop = document.querySelector('.backdrop')
 let modal = document.querySelector('.modal')
 let selectPlanBtn = backdrop.querySelectorAll('.plan button')
 
-for (let i = 0 ;i< selectPlanBtn.length; i++) {
+for (let i = 0; i < selectPlanBtn.length; i++) {
     selectPlanBtn[i].addEventListener('click', function() {
         modal.style.display = 'block'
         backdrop.style.display = 'block'
