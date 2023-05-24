4b2c5dc0f9e08d662c71403ad6d4d092215dfb2a
MaryamTalebpour <maryamtalebpourb@gmail.com>
2023-05-24 20:55:53 +0330

    .

--- scripts/shared.js
+++ scripts/shared.js
@@ -5,8 +5,10 @@ let modalCloseBtn = document.querySelector('.modal-action-negative')
 
 for (let i = 0; i < selectPlanBtn.length; i++) {
     selectPlanBtn[i].addEventListener('click', function() {
-        modal.style.display = 'block'
-        backdrop.style.display = 'block'
+        //modal.style.display = 'Block'
+        //backdrop.style.display = 'block'
+        modal.classList.add('open')
+        backdrop.classList.add('open')
     })
 }
 
@@ -14,7 +16,8 @@ backdrop.addEventListener('click' , closeModal )
 modalCloseBtn.addEventListener('click' , closeModal)
 
 function closeModal() {
-    backdrop.style.display = "none"
-    modal.style.display = "none"
-
+    //backdrop.style.display = "none"
+    //modal.style.display = "none"
+    modal.classList.remove('open')
+    backdrop.classList.remove('open')
 }
